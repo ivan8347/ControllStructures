acf5eb4c21b4bb8d29bcc389817f5a4773799296
ivan8347 <ivan8347@mail.ru>
2025-03-14 21:55:05 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<conio.h>
+#include<Windows.h>
+#include <iomanip>
+//#define BINARY
+//#define PASCAL
+#define ROMB          // ромб чуть-чуть проще ¬ашего
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::setw;
+
+void main()
+{
+    setlocale(LC_ALL, "");
+    int n;
+    cout << "¬ведите велечину пирамиды :";    
+    cin >> n;
+#ifdef ROMB
+    cout << endl;
+    for (int i = 0; i < n; i++)
+        cout << setw(n - i) << '/' << setw(i * 2 + 1) << '\\' << endl;
+    for (int i = n - 1; i >= 0; i--)
+        cout << setw(n - i) << '\\' << setw(i * 2 + 1) << '/' << endl;
+
+#endif // ROMB
+
+
+#ifdef PASCAL
+    for (int i = 0; i < n; i++)
+    {
+        int a = 1;
+        cout << setw(n - i);
+        for (int j = 0; j <= i; j++)
+        {
+            if (a < 10) cout << " " << a << " ";
+            else cout << a << " ";
+            a = a * (i - j) / (j + 1);
+
+        }
+        cout << endl;
+    }
+
+#endif // PASCAL
+
+
+                 
+#ifdef BINARY
+    cout << "Hex: " << hex << n << endl;
+    for (int i = 65536; i >= 1; i /= 2)
+    {
+        int  a = n / i;
+        n %= i;
+        cout << a << " ";
+    }   cout << endl;
+
+#endif // BINARY
+
+        
+}
+
+  
+        
+    
+    
+
+    
